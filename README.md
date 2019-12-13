fece52e0d62f9a25b8c84bfb7fc1fa3139a38eff
z15566664543 <1017994139@qq.com>
2019-12-13 22:31:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dormitoryManager
+## 这是一个基于SSM框架和MySQL数据库开发的一个学生宿舍管理系统
+
---
+++ src/main/java/com/itheima/dao/VisitorDao.java
@@ -0,0 +1,28 @@
+package com.itheima.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.itheima.po.Visitor;
+
+/**
+ * @program: dormitorySystem
+ * @description: 访客
+ * @author: zhouzhen
+ **/
+public interface VisitorDao {
+    /**
+     * 进行分页查询
+     */
+
+    //获取总条数
+	public Integer totalCount(@Param("v_name") String v_name, @Param("v_phone") String v_phone);
+    //获取用户列表
+	public List<Visitor> getVisitorList(@Param("v_name") String v_name, @Param("v_phone") String v_phone,
+			@Param("currentPage") Integer currentPage, @Param("pageSize") Integer pageSize);
+
+    public int addVisitor(Visitor visitor);   //添加学生信息
+    public List<Visitor> getAll();
+
+}
